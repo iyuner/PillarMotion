41ffeca90376ac53d88fa56f4a2c53cf7591ba6a
Ajinkya Khoche <khoche@kth.se>
2021-11-08 18:15:51 +0000

    install.sh: simplify, update to pytorch 1.7, solve pycocotools problem

--- install.sh
+++ install.sh
@@ -9,33 +9,34 @@ conda activate PillarMotion
 
 # torch 1.8, cuda 10.2
 # conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=10.2 -c pytorch --yes
-conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch --yes
+#conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch --yes
+conda install -y pytorch==1.7.0 torchvision==0.8.0 torchaudio==0.7.0 cudatoolkit=10.1 -c pytorch
 
 # install mmcv 1.3.9, which suitable for the torch and cuda versions
 # pip install mmcv-full==1.3.9 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.8.0/index.html
-pip install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.6.0/index.html
-
+#pip install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.6.0/index.html
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.7.0/index.html
 
 # install mmdetection 2.16.0
 # pip install mmdet==2.16.0
-pip install mmdet==2.17.0
+pip install mmdet
 
 # install mmsegmentation 0.17.0
 # pip install mmsegmentation==0.17.0
-pip install mmsegmentation==0.18.0
+pip install mmsegmentation
 
 # install mmdetection3d
 # pip install https://github.com/open-mmlab/mmdetection3d.git
 # if the upper fails, try this one
 
 # git submodule add https://github.com/open-mmlab/mmdetection3d.git
+git clone https://github.com/open-mmlab/mmdetection3d.git
 # git submodule init
-# git submodule update
+# git submodule update --recursive
 
-conda clean --all --yes
 cd mmdetection3d
 export FORCE_CUDA="1"
-pip install -r requirements/build.txt
+#pip install -r requirements/build.txt
 
 # not work in moria, fail with error:
 # raise EnvironmentError('CUDA_HOME environment variable is not set. '
@@ -48,5 +49,10 @@ pip install -r requirements/build.txt
 salloc --gres=gpu:1 --mem=2GB --cpus-per-task=4 --constrain=balrog
 conda activate PillarMotion
 pip install --no-cache-dir -e .
-pip install -e "git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI"
+#pip install -e "git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI"
+#Source : https://github.com/cocodataset/cocoapi/issues/509
+pip uninstall pycocotools 
+pip install pycocotools --no-cache-dir --no-binary :all:
+
 # then good!
+conda clean --all --yes
